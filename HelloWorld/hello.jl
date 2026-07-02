3af0797763695141d70e1ba0213fcf72fdc101c9
maker2413 <maker2413@gmail.com>
2026-07-01 21:28:01 -0500

    improved first section of Julia notes

---
+++ HelloWorld/hello.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/env julia
+println("Hello World!")
+
+# This is a single line comment.
+
+#=
+This is a multi line comment.
+These are made with '#=' and '=#'.
+=#
+
+# Setting up some variables
+first = "Dave"
+last = "Davidson"
+
+print("First: $first ")
+println("Last: $last")
+
+println("First: ", first, " Last: ", last)
--- HelloWorld/main.jl
+++
@@ -1,7 +0,0 @@
-# This is a single line comment
-
-#=
-This is what a multi line comments looks like.
-These a different syntax than some languages, but are still quite simple.
-=#
-println("Hello World!")
